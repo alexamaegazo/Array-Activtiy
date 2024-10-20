77b4ce5fb5719c3996cfe4613bd0303216ca0125
alexamaegazo <alexamaegazo5@gmail.com>
2024-10-20 10:48:09 +0800

    initial commit with arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+// Create an array called favSingers and store your three favorite singers in it
+
+const favSingers = ["Arthur Nery", "Adie", "Bruno Mars" ];
+console.log(favSingers[0]);
+
+const favNumbers = [25,9,11,18,21];
+
+const mixedArr =  ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
